8ffc674fccbadcb94ef8971495a87a7922880028
Heidi Mason <heidi@ipcprint.com>
2018-05-03 00:26:23 -0700

    feat: Convert db timestamp to user readable format

--- frontend/src/components/posts/index.js
+++ frontend/src/components/posts/index.js
@@ -4,8 +4,8 @@ import IconMoodGood from 'material-ui/svg-icons/social/mood'
 import IconMoodBad from 'material-ui/svg-icons/social/mood-bad'
 import Divider from 'material-ui/Divider'
 import Subheader from 'material-ui/Subheader'
-import Avatar from 'material-ui/Avatar';
 import { white, grey400, darkBlack, fullBlack } from 'material-ui/styles/colors'
+import Avatar from 'material-ui/Avatar'
 import IconButton from 'material-ui/IconButton'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import IconMenu from 'material-ui/IconMenu'
@@ -38,7 +38,17 @@ const rightIconMenu = (
 
 class ListPosts extends Component {
   render() {
-    const { posts } = this.props
+    const { posts } = this.props,
+
+            options = {
+              weekday: 'short',
+              year: '2-digit',
+              month: 'numeric',
+              day: 'numeric',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit'
+            }
 
     return (
       <List style={styles.list}>
@@ -46,12 +56,12 @@ class ListPosts extends Component {
           <div key={index}>
             <Subheader
               style={{color: fullBlack}}>
-              {post.timestamp}
+              { new Date(post.timestamp).toLocaleString([], options) }
             </Subheader>
 
             <ListItem
               disabled={false}
-              leftAvatar={<Avatar>{post.author.charAt(0)}</Avatar>}
+              leftAvatar={<Avatar>{ post.author.charAt(0) }</Avatar>}
               rightIconButton={rightIconMenu}
               rightIcon={
                 <div style={{width: '25%'}}>
