aec2ca57c9f6796d73c449cca04aadea04e66778
Heidi Mason <heidi@ipcprint.com>
2018-05-21 14:13:44 -0700

    feat: Add edit/delete functionality to comments

--- frontend/src/components/Comments/details.js
+++ frontend/src/components/Comments/details.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import {
-  // cyanA400,
-  // grey400, grey500,
-  grey400, fullBlack
-  // darkBlack, fullBlack
+  cyanA400,
+  grey400, grey500,
+  fullBlack
 } from 'material-ui/styles/colors'
-import { withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import IconButton from 'material-ui/IconButton'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import { List, ListItem } from 'material-ui/List'
@@ -15,12 +14,9 @@ import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import IconMoodGood from 'material-ui/svg-icons/social/mood'
 import IconMoodBad from 'material-ui/svg-icons/social/mood-bad'
-// import IconComment from 'material-ui/svg-icons/communication/comment'
-// import Divider from 'material-ui/Divider'
-// import ScrollableDialog from 'material-ui/Dialog'
-// import SelectField from 'material-ui/SelectField'
-// import TextField from 'material-ui/TextField'
-// import FlatButton from 'material-ui/FlatButton'
+import ScrollableDialog from 'material-ui/Dialog'
+import TextField from 'material-ui/TextField'
+import FlatButton from 'material-ui/FlatButton'
 import { connect } from 'react-redux'
 import { getComments, editComment, upvoteComment, downvoteComment, removeComment } from './actions'
 import serializeForm from 'form-serialize'
@@ -50,10 +46,6 @@ class CommentDetails extends Component {
     this.setState({modalOpen: false})
   }
 
-  changeTitle = e => {
-    this.setState({title: e.target.value})
-  }
-
   changeBody = e => {
     this.setState({body: e.target.value})
   }
@@ -65,7 +57,6 @@ class CommentDetails extends Component {
 
     const comment = Object.assign(values, {
       id: this.state.id,
-      title: this.state.title,
       body: this.state.body
     })
 
@@ -90,66 +81,122 @@ class CommentDetails extends Component {
             }
 
     return (
-      <List>
-        {comments.map( (comment, index) => (
-          <div key={index}>
-            <Subheader className="comment-subheader">
-              { new Date(comment.timestamp).toLocaleString([], options) }
+      <div>
+        <List>
+          {comments.map( (comment, index) => (
+            <div key={index}>
+              <Subheader className="comment-subheader">
+                { new Date(comment.timestamp).toLocaleString([], options) }
 
-              <div className="comment-icons">
-                <span className="vote-score">{comment.voteScore}</span>
+                <div className="comment-icons">
+                  <span className="vote-score">{comment.voteScore}</span>
 
-                <IconMoodGood
-                  className="icon-mood icon-mood-good"
-                  onClick={e => commentUpvote(comment)}
+                  <IconMoodGood
+                    className="icon-mood icon-mood-good"
+                    onClick={e => commentUpvote(comment)}
+                    />
+
+                  <IconMoodBad
+                    className="icon-mood"
+                    onClick={e => commentDownvote(comment)}
                   />
 
-                <IconMoodBad
-                  className="icon-mood"
-                  onClick={e => commentDownvote(comment)}
-                />
+                  <IconMenu
+                    iconButtonElement={iconButtonElement}
+                    style={{float: 'right'}}>
+                    <MenuItem style={{color: fullBlack}}
+                      onClick={ () => {
+                        this.setState({
+                          modalOpen: true,
+                          id: comment.id,
+                          body: comment.body,
+                          author: comment.author
+                        })
+                      }}>Edit
+                    </MenuItem>
 
-                <IconMenu
-                  iconButtonElement={iconButtonElement}
-                  style={{float: 'right'}}>
-                  <MenuItem style={{color: fullBlack}}
-                    onClick={ () => {
-                      this.setState({
-                        modalOpen: true,
-                        id: comment.id,
-                        title: comment.title,
-                        body: comment.body,
-                        author: comment.author,
-                        category: comment.category
-                      })
-                    }}>Edit
-                  </MenuItem>
+                    { /* TODO: Add confirmation dialog */ }
+                    <MenuItem
+                      style={{color: fullBlack}}
+                      onClick={e => commentRemove(comment)}>Delete
+                    </MenuItem>
+                  </IconMenu>
+                </div>
+              </Subheader>
 
-                  { /* TODO: Add confirmation dialog */ }
-                  <MenuItem
-                    style={{color: fullBlack}}
-                    onClick={e => commentRemove(comment)}>Delete
-                  </MenuItem>
-                </IconMenu>
+              <ListItem
+                value={3}
+                disabled={true}
+                primaryText={comment.author}
+                secondaryText={
+                  <p>
+                    <span style={{color: fullBlack}}>{comment.body}</span><br />
+                  </p>
+                }
+                secondaryTextLines={2}
+                leftAvatar={<Avatar>{ comment.author ? comment.author.charAt(0) : null }</Avatar>}
+                style={{color: fullBlack}}
+              />
+            </div>
+          ))}
+        </List>
+
+        {comments.map( (comment, index) => (
+          <ScrollableDialog
+            title="Edit Comment"
+            actions={
+              <div>
+                <Link to={window.location.pathname}>
+                  <FlatButton
+                    label="Cancel"
+                    primary={true}
+                    onClick={this.closeModal}
+                    style={{marginRight: 15}}
+                  />
+                </Link>
+
+                <Link to={window.location.pathname}>
+                  <FlatButton
+                    label="Submit"
+                    primary={true}
+                    keyboardFocused={true}
+                    onClick={this.editComment}
+                    backgroundColor={cyanA400}
+                    hoverColor={cyanA400}
+                  />
+                </Link>
               </div>
-            </Subheader>
+            }
+            modal={false}
+            open={this.state.modalOpen}
+            onRequestClose={this.closeModal}
+            autoScrollBodyContent={true}
+            titleStyle={{color: fullBlack}}
+            key={index}>
+            <form>
+              <TextField
+                floatingLabelText="Author"
+                inputStyle={{color: fullBlack}}
+                value={this.state.author}
+                fullWidth={true}
+                disabled={true}
+              />
 
-            <ListItem
-              value={3}
-              disabled={true}
-              primaryText={comment.author}
-              secondaryText={
-                <p>
-                  <span style={{color: fullBlack}}>{comment.body}</span><br />
-                </p>
-              }
-              secondaryTextLines={2}
-              leftAvatar={<Avatar>{ comment.author ? comment.author.charAt(0) : null }</Avatar>}
-              style={{color: fullBlack}}
-            />
-          </div>
+              <TextField
+                floatingLabelText="Message"
+                floatingLabelStyle={{color: grey500}}
+                textareaStyle={{color: fullBlack}}
+                multiLine={true}
+                rows={2}
+                rowsMax={4}
+                fullWidth={true}
+                value={this.state.body}
+                onChange={this.changeBody}
+              />
+            </form>
+          </ScrollableDialog>
         ))}
-      </List>
+      </div>
     )
   }
 }
