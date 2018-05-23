d7204adba000d3ffdf4f615d16d96be85f76e594
Heidi Mason <heidi@ipcprint.com>
2018-05-23 13:48:59 -0700

    feat: Add edit/delete to list view for posts

--- frontend/src/components/posts/index.js
+++ frontend/src/components/posts/index.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import '../../css/Posts.css'
 import {
+  cyanA400,
   blueGrey100,
+  grey400, grey500,
   darkBlack, fullBlack
 } from 'material-ui/styles/colors'
+import IconButton from 'material-ui/IconButton'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import { List, ListItem } from 'material-ui/List'
 import Subheader from 'material-ui/Subheader'
 import Avatar from 'material-ui/Avatar'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
 import IconMoodGood from 'material-ui/svg-icons/social/mood'
 import IconMoodBad from 'material-ui/svg-icons/social/mood-bad'
 import Divider from 'material-ui/Divider'
-import { withRouter } from 'react-router-dom'
+import ScrollableDialog from 'material-ui/Dialog'
+import SelectField from 'material-ui/SelectField'
+import TextField from 'material-ui/TextField'
+import FlatButton from 'material-ui/FlatButton'
+import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { upvotePost, downvotePost } from './actions'
+import { editPost, upvotePost, downvotePost, removePost } from './actions'
+import serializeForm from 'form-serialize'
 
 const styles = {
   listItem: {
@@ -20,17 +31,53 @@ const styles = {
   }
 }
 
+const iconButtonElement = (
+  <IconButton
+    touch={true}
+    tooltip="more"
+    tooltipPosition="bottom-left"
+  >
+    <MoreVertIcon color={grey400} />
+  </IconButton>
+)
+
 class ListPosts extends Component {
   state = {
-    modalOpen: false
+    postModalOpen: false
   }
 
-  closeModal = () => {
-    this.setState({modalOpen: false})
+  closePostModal = () => {
+    this.setState({postModalOpen: false})
+  }
+
+  changeTitle = e => {
+    this.setState({title: e.target.value})
+  }
+
+  changeBody = e => {
+    this.setState({body: e.target.value})
+  }
+
+  editPost = e => {
+    e.preventDefault()
+
+    const values = serializeForm(e.target, { hash: true })
+
+    const post = Object.assign(values, {
+      id: this.state.id,
+      title: this.state.title,
+      body: this.state.body
+    })
+
+    // Dispatch action
+    this.props.postEdit(post)
+
+    // Close modal upon submitting form
+    this.closePostModal()
   }
 
   render() {
-    const { showingPosts, history, upvote, downvote } = this.props,
+    const { showingPosts, history, upvote, downvote, postRemove } = this.props,
 
             options = {
               weekday: 'short',
@@ -62,6 +109,29 @@ class ListPosts extends Component {
                   className="icon-mood"
                   onClick={e => downvote(post)}
                 />
+
+                <IconMenu
+                  iconButtonElement={iconButtonElement}
+                  style={{float: 'right'}}>
+                  <MenuItem style={{color: fullBlack}}
+                    onClick={ () => {
+                      this.setState({
+                        postModalOpen: true,
+                        id: post.id,
+                        title: post.title,
+                        body: post.body,
+                        author: post.author,
+                        category: post.category
+                      })
+                    }}>Edit
+                  </MenuItem>
+
+                  { /* TODO: Add confirmation dialog */ }
+                  <MenuItem
+                    style={{color: fullBlack}}
+                    onClick={e => postRemove(post)}>Delete
+                  </MenuItem>
+                </IconMenu>
               </div>
             </Subheader>
 
@@ -87,6 +157,83 @@ class ListPosts extends Component {
             {showingPosts.length > 1 &&
               <Divider inset={false} />
             }
+
+            {showingPosts.map( (post, index) => (
+              <ScrollableDialog
+                title="Edit Post"
+                actions={
+                  <div>
+                    <Link to={`/${post.category}`}>
+                      <FlatButton
+                        label="Cancel"
+                        primary={true}
+                        onClick={this.closePostModal}
+                        style={{marginRight: 15}}
+                      />
+                    </Link>
+
+                    <Link to={`/${post.category}`}>
+                      <FlatButton
+                        label="Submit"
+                        primary={true}
+                        keyboardFocused={true}
+                        onClick={this.editPost}
+                        backgroundColor={cyanA400}
+                        hoverColor={cyanA400}
+                      />
+                    </Link>
+                  </div>
+                }
+                modal={false}
+                open={this.state.postModalOpen}
+                onRequestClose={this.closePostModal}
+                autoScrollBodyContent={true}
+                titleStyle={{color: fullBlack}}
+                key={index}>
+                  <form>
+                    <SelectField
+                      floatingLabelText="Category"
+                      value={this.state.category}
+                      autoWidth={true}
+                      menuItemStyle={{color: fullBlack}}
+                      className="select-category"
+                      disabled={true}>
+                      <MenuItem value="react" primaryText="React" />
+                      <MenuItem value="redux" primaryText="Redux" />
+                      <MenuItem value="udacity" primaryText="Udacity" />
+                    </SelectField>
+
+                    <TextField
+                      floatingLabelText="Title"
+                      floatingLabelStyle={{color: grey500}}
+                      inputStyle={{color: fullBlack}}
+                      value={this.state.title}
+                      onChange={this.changeTitle}
+                      className="title-input"
+                    />
+
+                    <TextField
+                      floatingLabelText="Author"
+                      inputStyle={{color: fullBlack}}
+                      style={{marginLeft: 15}}
+                      value={this.state.author}
+                      disabled={true}
+                    />
+
+                    <TextField
+                      floatingLabelText="Message"
+                      floatingLabelStyle={{color: grey500}}
+                      textareaStyle={{color: fullBlack}}
+                      multiLine={true}
+                      rows={2}
+                      rowsMax={4}
+                      fullWidth={true}
+                      value={this.state.body}
+                      onChange={this.changeBody}
+                    />
+                  </form>
+                </ScrollableDialog>
+              ))}
           </div>
         ))}
       </List>
@@ -96,8 +243,10 @@ class ListPosts extends Component {
 
 const mapDispatchToProps = dispatch => {
   return {
+    postEdit: p => dispatch( editPost(p) ),
     upvote: p => dispatch( upvotePost(p) ),
-    downvote: p => dispatch( downvotePost(p) )
+    downvote: p => dispatch( downvotePost(p) ),
+    postRemove: p => dispatch( removePost(p) )
   }
 }
 
